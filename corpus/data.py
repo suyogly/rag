16751b7a0e84f60d2c34cad429c4b4ca9b1ccad5
suyogly <bizpurpose09@gmail.com>
2026-04-16 11:18:15 +0545

    first data

---
+++ corpus/data.py
@@ -0,0 +1,20 @@
+'''
+This module contains the sentences
+which will be used to create vectors
+from sentence-transformers. And will 
+be used to check the similarity between the sentences.
+'''
+
+
+_SENTENCES = [
+    "The cat is on the table.",
+    "The dog is in the garden.",
+    "The cat is sleeping on the sofa.",
+    "The dog is barking loudly.",
+    "The cat is playing with a ball.",
+    "The dog is running in the park.",
+    "The cat is eating a mouse.",
+    "The dog is chasing a squirrel.",
+    "The cat is purring softly.",
+    "The dog is wagging its tail."
+]
