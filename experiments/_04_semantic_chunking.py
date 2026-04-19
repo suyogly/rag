62c69a6e9aa876222e52d779447740a9820476be
suyogly <bizpurpose09@gmail.com>
2026-04-20 00:25:33 +0545

    try: semantic chunking first try, needs to know the inner working of an embedding model to know its behaviour

---
+++ experiments/_04_semantic_chunking.py
@@ -0,0 +1,34 @@
+from corpus.loader import load_document
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.core import Document
+from llama_index.core.node_parser import (
+    SentenceSplitter,
+    SemanticSplitterNodeParser
+)
+
+_EMBEDDING_MODEL = HuggingFaceEmbedding("BAAI/bge-small-en-v1.5")
+
+langchain_doc = load_document()
+
+# changing Langchain Document to Llamaindex supported Document
+docs = [
+    Document(
+        text = doc.page_content,
+        metadata = doc.metadata
+    ) 
+    for doc in langchain_doc
+]
+
+splitter = SemanticSplitterNodeParser(
+    buffer_size=1, 
+    breakpoint_percentile_threshold=95, 
+    embed_model=_EMBEDDING_MODEL
+)
+
+nodes = splitter.get_nodes_from_documents(docs)
+
+for idx, node in enumerate(nodes):
+    print(f"id: {idx} \n{node.text}")
+    print("\n---\n")
+
+
