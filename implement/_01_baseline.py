e60ace7b8ec9fe30a4db952ec0f7a7028f0a175b
suyogly <bizpurpose09@gmail.com>
2026-05-13 06:12:43 +0545

    baseline start

--- implement/_01_baseline.py
+++ implement/_01_baseline.py
@@ -1,4 +1,5 @@
 from corpus.loader import load_document
 from shared.chunker import fixed_splitter
 from shared.embedder import embed_with_BGE_384
-from shared.llm_client import chat_groq
+from shared.llm_client import groq_gpt_oss_120b
+
